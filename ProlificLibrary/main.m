248a8ad351f3a0238d1f82b63a1ec81d7a1b56e6
Albert Lardizabal <albie06@gmail.com>
2014-09-02 15:55:52 -0400

    first commit

---
+++ ProlificLibrary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ProlificLibrary
+//
+//  Created by Albert Lardizabal on 9/2/14.
+//  Copyright (c) 2014 Albert Lardizabal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AALAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AALAppDelegate class]));
+    }
+}
